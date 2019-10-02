c30eaa16e7f3a5da8de5173ae5043aca2fd92e1a
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2019-10-01 23:28:52 -0700

    submit a new playlist with method post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,11 +7,17 @@ playlists = db.playlists
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return render_template('home.html', msg='Flask is Cool!!')
-
-@app.route('/playlists')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    print(request.form.to_dict())
+    return redirect(url_for('playlists_index'))
