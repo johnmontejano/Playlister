cf0b9c9c3819ba153048c58969a7a5576ab6a0f8
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2019-10-01 23:59:04 -0700

    show playlist id

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
@@ -26,3 +27,8 @@ def playlists_submit():
     }
     playlists.insert_one(playlist)
     return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    return f'My ID is {playlist_id}'
